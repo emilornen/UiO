029f0f74aa2e4ccac4f0b9d8dc1899bc47096175
emilornen <emilornes@gmail.com>
2025-09-03 18:00:36 +0200

    gjorde noen gode endringer, chat!

---
+++ Python/Oppgaver/odd.py
@@ -0,0 +1,9 @@
+# lage en løkke (while) for oddetall. UENDELIG STOR ALDRI SLUTTE ØDELEGGE DATA
+
+m = 1
+n = 22
+
+while m <= n:
+    if m % 2 != 0:
+        print(m)
+    m += 1
--- Python/Oppgaver/primes.py
+++ Python/Oppgaver/primes.py
@@ -1,10 +1,10 @@
 #lag en liste med primtall i rekkefølge
 
-tissetass = [2, 3, 5, 7, 11, 13]
+primes = [2, 3, 5, 7, 11, 13]
 p = 17
 
-for tissetasses in tissetass:
-    print(tissetasses)
+for prime in primes:
+    print(prime)
 
-tissetass.append(p)
-print(tissetass[-1])
+primes.append(p)
+print(primes[-1])
